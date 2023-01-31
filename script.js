18745e3d5390e14b225f42ef51b0a813fd5357e7
Nick <harmoniacodes@proton.me>
2023-01-31 11:10:02 -0500

    added password length check

--- script.js
+++ script.js
@@ -2,9 +2,48 @@
 
 // Create the generatePassword function that is called in writePassword()
 function generatePassword() {
-  console.log("Generate button clicked");
-  newPassword = "This will be a password soon";
-  return newPassword;
+  // Ask the user to set a password length
+  var length = prompt('Choose a password length between 8-128 characters.');
+  // check to see if length input is Not a Number (NaN)
+  if (isNaN(length)) {
+    var newPassword = "Invalid characters. Please input a number.";
+    return newPassword;
+  }
+  // Check that length input is between 8-128
+  else if (length < 8 || length > 128) {
+    var newPassword = "Error. Please choose a number between 8-128."
+    return newPassword;
+  } else {
+    // Ask the user for password options
+    var caps = confirm('Capital Letters?');
+    if (caps = true) {
+      return newPassword = "capitals!"
+    };
+    var uppercase = confirm('Uppercase?');
+    var lowercase = confirm('Lowercase?');
+    var special = confirm('Special Characters?');
+    return newPassword = "Time for the next part."
+  }
+  // } else if (typeof length !== NaN && length < 8 || length > 128) {
+  //   var newPassword = "Continue";
+  //   var caps = confirm('Capital Letters?');
+  //   var uppercase = confirm('Uppercase?');
+  //   var lowercase = confirm('Lowercase?');
+  //   var special = confirm('Special Characters?');
+  //   return newPassword;
+  // }
+
+  // Ask the user for password options
+  // var caps = confirm('Capital Letters?');
+  // var uppercase = confirm('Uppercase?');
+  // var lowercase = confirm('Lowercase?');
+  // var special = confirm('Special Characters?');
+  // console.log("Generate button clicked");
+
+
+
+  // newPassword = "This will be a password soon";
+  // return newPassword;
 }
 
 // Get references to the #generate element
